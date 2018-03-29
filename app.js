9ecf66fcb4670e5e1d01edae77ccaea0b884370e
yangtao123455 <1025066903@qq.com>
2018-03-29 18:34:23 +0800

    加入jwt验证

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var parseurl = require('parseurl');
+var RedisStore = require('connect-redis')(session)
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,7 +14,7 @@ var users = require('./routes/users');
 var app = express();
 app.use("*", function (req, res, next) { //跨域的解决方法
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,X-Token");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     if (req.method === 'OPTIONS') {
         res.sendStatus(200)
@@ -19,6 +22,7 @@ app.use("*", function (req, res, next) { //跨域的解决方法
         next()
     }
 });
+
 // view engine setup
 var ejs = require('ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +35,43 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    store: new RedisStore({
+        host: '127.0.0.1',
+        port: 6379,
+    }),
+    resave: false,  //重新保存
+    saveUninitialized: true, //
+    secret: 'express admin',//通过设置的 secret 字符串，来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改。
+    cookie:{ maxAge: 1000*60*60*24}//失效时间
+
+}));
+/*app.use(function (req, res, next) {
+    var token = req.session.token;
+    var pathname = parseurl(req).pathname;
+    console.log(req.session)
+    console.log(pathname)
+    //判断是否登录入
+    if(!token){
+        //未登录清空token输出给模板值
+        token = req.session.token = null;
+        res.locals.user=null;
+        app.locals.user=null;
+        //除了login的路径其他都要登录后才能进入
+        if(/^\/login/g.test(pathname)){
+            //跳出
+            next()
+        }else{
+            res.json({status: 2, msg: '登录超时，请重新登录'});
+        }
+    }else if(token){
+        //已登录
+        //输出值到模板，res.locals，不前页面输出，app.locals是模板全局出输
+        res.locals.user=JSON.parse(token);
+        app.locals.user=JSON.parse(token);
+        next();
+    }
+})*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var  token = require('../utils/token');
 
 var mysql = require('mysql');
 var db = require('../config/db');
 var pool = mysql.createPool(db.mysql);
 const utils = require('../utils/responseJson');
-console.log(utils)
+const crypto = require('crypto');
 /* GET home page. */
 router.post('/login/login', function(req, res, next) {
-    pool.query('select * from admin_table', function (err, raws) {
-        if(err) {
-            throw err;
-        }
-        console.log(raws[0])
-        utils.responseJONS(res, raws[0]);
+    const userData = {
+        username:req.body.username,
+        password:req.body.password
+    }
+    const myToken = token.createToken(userData, 60)
+    res.json({
+        status: 1,
+        token: myToken,
+        msg: '登录成功！'
     })
 });
 
 router.post('/getUserInfo', function (req, res, next) {
+    if(req.method != 'OPTIONS') {
+        var mytoken = req.headers['x-token']
+        console.log('token',mytoken)
+        console.log(token.checkToken(mytoken))
+    }
     res.send({roles:['admin']})
 })
 

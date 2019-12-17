68be6aa0fd2bf1aa98a40ad4fcf2f0456932f3c1
tao.yang <tao.yang@ihandysoft.com>
2019-12-17 10:28:16 +0800

    代码格式化修改

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var  token = require('../utils/token');
+var token = require('../utils/token');
 
 var mysql = require('mysql');
 var db = require('../config/db');
@@ -11,256 +11,164 @@ var moment = require('moment')
 /* GET home page. */
 
 function MD5(str) {
-    let hash = crypto.createHash('md5')
-    let hashed = hash.update(str)
-    return hashed.digest('base64')
+  let hash = crypto.createHash('md5')
+  let hashed = hash.update(str)
+  return hashed.digest('base64')
 }
 
-router.post('/login/login', function(req, res, next) {
+router.post('/login/login', function (req, res, next) {
 
-    const userData = {
-        username:req.body.username,
-        password:req.body.password
-    }
-    const hashPassword = MD5(userData.password)
-    const myToken = token.createToken(userData, 60*60*24)
-    req.getConnection(function (err, conn) {
-        if(err) {
-            return next(err);
+  const userData = {
+    username: req.body.username,
+    password: req.body.password
+  }
+  const hashPassword = MD5(userData.password)
+  const myToken = token.createToken(userData, 60 * 60 * 24)
+  req.getConnection(function (err, conn) {
+    if (err) {
+      return next(err);
+    } else {
+      conn.query(`SELECT * FROM users WHERE username= '${userData.username}'`, [], function (err, result) {
+        if (err) {
+          res.json({
+            status: 0,
+            msg: '数据库查询连接出错'
+          })
+        } else if (result.length == 0) {
+
+          conn.query(`INSERT INTO users (username, password) VALUE ('${userData.username}','${hashPassword}')`, function (err, result) {
+            if (err) {
+              res.json({
+                status: 0,
+                msg: '数据库查询连接出错'
+              })
+            } else {
+              req.session.username = req.body.username
+              res.json({
+                status: 1,
+                token: myToken,
+                msg: '登录成功！'
+              })
+            }
+          })
         } else {
-            conn.query(`SELECT * FROM users WHERE username= '${userData.username}'`, [], function (err,result) {
-                if(err) {
-                    res.json({
-                        status: 0,
-                        msg: '数据库查询连接出错'
-                    })
-                } else if(result.length == 0) {
-
-                    conn.query(`INSERT INTO users (username, password) VALUE ('${userData.username}','${hashPassword}')`, function(err, result) {
-                        if(err) {
-                            res.json({
-                                status: 0,
-                                msg: '数据库查询连接出错'
-                            })
-                        } else {
-                            req.session.username = req.body.username
-                            res.json({
-                                status: 1,
-                                token: myToken,
-                                msg: '登录成功！'
-                            })
-                        }
-                    })
-                } else {
-                    var savePass = result[0].password.toString();
-                    if(savePass != hashPassword.toString()) {
-                        res.json({
-                            status: 0,
-                            msg: '用户名或者密码错误！'
-                        })
-                    } else {
-
-                        req.session.username = req.body.username
-                        res.json({
-                            status: 1,
-                            token: myToken,
-                            msg: '登录成功！'
-                        })
-                    }
-                }
-
+          var savePass = result[0].password.toString();
+          if (savePass != hashPassword.toString()) {
+            res.json({
+              status: 0,
+              msg: '用户名或者密码错误！'
             })
+          } else {
 
+            req.session.username = req.body.username
+            res.json({
+              status: 1,
+              token: myToken,
+              msg: '登录成功！'
+            })
+          }
         }
-    })
+
+      })
+
+    }
+  })
 
 });
 
 router.post('/getUserInfo', function (req, res, next) {
 
 
-    res.send({roles:[req.session.username],status: 1})
+  res.send({ roles: [req.session.username], status: 1 })
 })
 
 // function dateFormat(date, fmt) {     if (null == date || undefined == date) return '';     var o = {         "M+": date.getMonth() + 1, //月份         "d+": date.getDate(), //日         "h+": date.getHours(), //小时         "m+": date.getMinutes(), //分         "s+": date.getSeconds(), //秒         "S": date.getMilliseconds() //毫秒     };     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));     for (var k in o)         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));     return fmt; } Date.prototype.toJSON = function () { return dateFormat(this,'yyyy-MM-dd hh:mm:ss')}
 router.post('/role/getRoleList', function (req, res, next) {
-    const pagesize = req.body.pagesize || 10;
-    const page = req.body.page || 1;
-    const point = (page - 1) * 10;
-    const user = req.body.user;
-    const address = req.body.address;
-    let sql = `SELECT * FROM users WHERE id <= 
+  const pagesize = req.body.pagesize || 10;
+  const page = req.body.page || 1;
+  const point = (page - 1) * 10;
+  const user = req.body.user;
+  const address = req.body.address;
+  let sql = `SELECT * FROM users WHERE id <= 
                 (SELECT id FROM users ORDER BY id desc LIMIT ${point}, 1) ORDER BY id desc LIMIT ${pagesize}`
-    let numSql = `SELECT count(*) AS total FROM users`
-    if(user && address) {
-        sql = `SELECT * FROM users WHERE id <= 
+  let numSql = `SELECT count(*) AS total FROM users`
+  if (user && address) {
+    sql = `SELECT * FROM users WHERE id <= 
                 (SELECT id FROM users ORDER BY id desc LIMIT ${point}, 1) AND username = '${user}' AND address = '${address}' ORDER BY id desc LIMIT ${pagesize}`
-        numSql = `SELECT count(*) AS total FROM users WHERE username='${user}' AND address='${address}'`
-    } else{
-        if(user) {
-            sql = `SELECT * FROM users WHERE id <= 
+    numSql = `SELECT count(*) AS total FROM users WHERE username='${user}' AND address='${address}'`
+  } else {
+    if (user) {
+      sql = `SELECT * FROM users WHERE id <= 
                 (SELECT id FROM users ORDER BY id desc LIMIT ${point}, 1) AND username = '${user}'  ORDER BY id desc LIMIT ${pagesize}`
-            numSql = `SELECT count(*) AS total FROM users WHERE username = '${user}'`
-        }
-        if(address){
-          sql = `SELECT * FROM users WHERE id <= 
-                (SELECT id FROM users ORDER BY id desc LIMIT ${point}, 1) AND address = '${address}'  ORDER BY id desc LIMIT ${pagesize}`
-          numSql = `SELECT count(*) AS total FROM users WHERE address = '${address}'`
-        }
+      numSql = `SELECT count(*) AS total FROM users WHERE username = '${user}'`
     }
-    req.getConnection(function (err, conn) {
-        if(err) {
-            next(err)
-        } else {
-            let resultPromise = new Promise((resolve,reject) => {
-              conn.query(sql,function (err,result) {
-                if(err) {
-                  reject(err)
-                } else {
-                  for(var i = 0; i < result.length; i ++) {
-                    result[i].updatetime = moment(result[i].updatetime).format('YYYY-MM-DD HH:mm:ss')
-                    result[i].createdate = moment(result[i].createdate).format('YYYY-MM-DD HH:mm:ss')
-                  }
-                  resolve(result)
-                }
+    if (address) {
+      sql = `SELECT * FROM users WHERE id <= 
+                (SELECT id FROM users ORDER BY id desc LIMIT ${point}, 1) AND address = '${address}'  ORDER BY id desc LIMIT ${pagesize}`
+      numSql = `SELECT count(*) AS total FROM users WHERE address = '${address}'`
+    }
+  }
+  req.getConnection(function (err, conn) {
+    if (err) {
+      next(err)
+    } else {
+      let resultPromise = new Promise((resolve, reject) => {
+        conn.query(sql, function (err, result) {
+          if (err) {
+            reject(err)
+          } else {
+            for (var i = 0; i < result.length; i++) {
+              result[i].updatetime = moment(result[i].updatetime).format('YYYY-MM-DD HH:mm:ss')
+              result[i].createdate = moment(result[i].createdate).format('YYYY-MM-DD HH:mm:ss')
+            }
+            resolve(result)
+          }
 
-              })
-            })
-            let totalPromise = new Promise((resolve, reject) => {
-              conn.query(numSql, function (err,total) {
-                if(err) {
-                    reject(err)
-                } else {
-                    resolve(total)
-                }
-              })
-            })
-            Promise.all([resultPromise, totalPromise]).then(function (results) {
-              res.json({
-                status: 1,
-                total: results[1][0].total,
-                tableData:results[0],
-                rowNum: pagesize
-              })
-            }).catch(function (err) {
-              res.json({
-                status: 0,
-                msg: '连接数据库或查询失败'
-              })
-              throw err
-            })
-            /*new Promise(resolve => {
-              conn.query(sql,function (err,result) {
-                  console.log('结果',result)
-                  for(var i = 0; i < result.length; i ++) {
-                    result[i].updatetime = moment(result[i].updatetime).format('YYYY-MM-DD HH:mm:ss')
-                    result[i].createdate = moment(result[i].createdate).format('YYYY-MM-DD HH:mm:ss')
-                  }
-                  resolve(result)
-              })
-            })
-              .then(function (result) {
-                console.log(22222222222222)
-                console.log('传到第二个结果',result)
-                return conn.query(numSql, function (err,total) {
-                  console.log('mytotal',total)
-                  res.json({
-                    status: 1,
-                    total: total[0].total,
-                    tableData:result,
-                    rowNum: pagesize
-                  })
-                })
-              })
-              .then(function (result) {
+        })
+      })
+      let totalPromise = new Promise((resolve, reject) => {
+        conn.query(numSql, function (err, total) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(total)
+          }
+        })
+      })
+      Promise.all([resultPromise, totalPromise]).then(function (results) {
+        res.json({
+          status: 1,
+          total: results[1][0].total,
+          tableData: results[0],
+          rowNum: pagesize
+        })
+      }).catch(function (err) {
+        res.json({
+          status: 0,
+          msg: '连接数据库或查询失败'
+        })
+        throw err
+      })
+    }
 
-              })
-              .catch(err => {
-                  res.json({
-                      status: 0,
-                      msg: '连接数据库或查询失败'
-                  })
-                  throw(err)
-              })*/
-            /*conn.query(sql,function (err,result) {
-                if(err) {
-                    console.log(err)
-                    res.json({
-                        status: 0,
-                        msg: '连接数据库或查询失败'
-                    })
-                } else {
-
-                    console.log('结果',result)
-                    for(var i = 0; i < result.length; i ++) {
-                        result[i].updatetime = moment(result[i].updatetime).format('YYYY-MM-DD HH:mm:ss')
-                        result[i].createdate = moment(result[i].createdate).format('YYYY-MM-DD HH:mm:ss')
-                    }
-                   conn.query(numSql, function (err,total) {
-                       console.log('mytotal',total)
-                       res.json({
-                           status: 1,
-                           total: total[0].total,
-                           tableData:result,
-                           rowNum: pagesize
-                       })
-                   })
-
-                }
-            })*/
-        }
-
-    })
-    /*res.json({
-        status: 1,
-        total: 4,
-        tableData:[{
-            date: '2016-05-03',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-        }, {
-            date: '2016-05-02',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-        }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-        }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-        }]
-    })*/
+  })
 })
 
-router.post('/role/roleEdit',function(req, res, next){
+router.post('/role/roleEdit', function (req, res, next) {
   let id = [req.body.id];
   let sql = `SELECT username FROM users WHERE id = ?`
-  req.getConnection(function(err, conn){
-    if(err) {
+  req.getConnection(function (err, conn) {
+    if (err) {
       next(err)
-    } else{
-      conn.query(sql, id, function (err,result) {
-        if(err) {
+    } else {
+      conn.query(sql, id, function (err, result) {
+        if (err) {
           next(err)
         } else {
           console.log(result[0])
-          if(result.length == 0 || result[0].username == req.body.username) {
+          if (result.length == 0 || result[0].username == req.body.username) {
             next()
-          } else{
+          } else {
             res.json({
               status: 0,
               msg: '用户名已存在！'
@@ -271,7 +179,7 @@ router.post('/role/roleEdit',function(req, res, next){
       })
     }
   })
-},function (req, res, next) {
+}, function (req, res, next) {
   const updateData = req.body
   let updatetime = moment(updateData.updatetime).format('YYYY-MM-DD HH:mm:ss')
   console.log(updateData)
@@ -285,11 +193,11 @@ router.post('/role/roleEdit',function(req, res, next){
              roles='${updateData.roles}' 
              WHERE id=${updateData.id}`
   req.getConnection(function (err, conn) {
-    if(err) {
+    if (err) {
       next(err)
     } else {
       conn.query(sql, function (err, result) {
-        if(err) {
+        if (err) {
           next(err)
         } else {
           console.log(result)
@@ -304,20 +212,20 @@ router.post('/role/roleEdit',function(req, res, next){
 
 })
 
-router.post('/role/roleAdd',function(req, res, next){
+router.post('/role/roleAdd', function (req, res, next) {
   let username = [req.body.username];
   let sql = `SELECT username FROM users WHERE username = ?`
-  req.getConnection(function(err, conn){
-    if(err) {
+  req.getConnection(function (err, conn) {
+    if (err) {
       next(err)
-    } else{
-      conn.query(sql,username, function (err,result) {
-        if(err) {
+    } else {
+      conn.query(sql, username, function (err, result) {
+        if (err) {
           next(err)
         } else {
-          if(result.length == 0) {
+          if (result.length == 0) {
             next()
-          } else{
+          } else {
             res.json({
               status: 0,
               msg: '用户名已存在！'
@@ -328,14 +236,14 @@ router.post('/role/roleAdd',function(req, res, next){
       })
     }
   })
-},function (req, res, next) {
+}, function (req, res, next) {
   const addData = req.body
   let addTime = moment(addData.updatetime).format('YYYY-MM-DD HH:mm:ss')
   console.log(addData)
   let hashPassword = MD5(addData.password)
 
-  let dataArr = [addData.username,addData.address,addData.city,addData.roles,addData.province,addTime,addTime,addData.zip,hashPassword]
-  let sql =  `INSERT INTO users
+  let dataArr = [addData.username, addData.address, addData.city, addData.roles, addData.province, addTime, addTime, addData.zip, hashPassword]
+  let sql = `INSERT INTO users
               (
                 username,
                 address,
@@ -350,11 +258,11 @@ router.post('/role/roleAdd',function(req, res, next){
               VALUES(?,?,?,?,?,?,?,?,?)`
 
   req.getConnection(function (err, conn) {
-    if(err) {
+    if (err) {
       next(err)
     } else {
-      conn.query(sql,dataArr, function (err, result) {
-        if(err) {
+      conn.query(sql, dataArr, function (err, result) {
+        if (err) {
           next(err)
           res.json({
             status: 0,
